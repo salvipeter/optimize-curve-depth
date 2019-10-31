f0371092077a432ee77c417a9149eaa4c8930a91
Peter Salvi <vukung@yahoo.com>
2019-10-31 15:00:40 +0100

    New concept, but still buggy

--- optimizer.cc
+++ optimizer.cc
@@ -4,13 +4,21 @@
 
 void optimize(BSplineCurve &curve, const VectorVector &directions, size_t n_samples, size_t fix) {
   size_t n = curve.n + 1, p = curve.p, m = n_samples;
-  size_t nv = n - fix * 2;
+
+  // Boundary constraint setup
+  std::vector<size_t> boundary;
+  for (size_t i = 0; i < fix; ++i) {
+    boundary.push_back(i);
+    boundary.push_back(n - i - 1);
+  }
+  size_t nb = boundary.size();
+  auto boundary_weights = DoubleVector(nb, 0);
 
   double knot_start = curve.knots.front();
   double knot_length = curve.knots.back() - curve.knots.front();
 
-  Eigen::MatrixXd A = Eigen::MatrixXd::Zero(m * nv, nv);
-  Eigen::VectorXd b = Eigen::VectorXd::Zero(m * nv);
+  Eigen::MatrixXd A = Eigen::MatrixXd::Zero(n + nb, n + nb);
+  Eigen::VectorXd b = Eigen::VectorXd::Zero(n + nb);
 
   for (size_t i = 0; i < m; ++i) { // sampled data point at u_i
     // Compute the basis function derivatives
@@ -20,25 +28,24 @@ void optimize(BSplineCurve &curve, const VectorVector &directions, size_t n_samp
     DoubleMatrix der;
     curve.basisFunctionDerivatives(span, u, 2, der);
 
-    for (size_t j = fix; j < n - fix; ++j) { // derivative by w_j
-      size_t jv = j - fix;
+    for (size_t j = span - p; j <= span; ++j) // derivative by w_j
+      for (size_t k = span - p; k <= span; ++k) { // coefficient of w_k
+        A(j, k) += directions[k] * directions[j] * 2 * der[2][k+p-span] * der[2][j+p-span];
+        b(j) -= curve.cp[k] * directions[j] * 2 * der[2][k+p-span] * der[2][j+p-span];
+      }
+  }
 
-      // Outside of the span?
-      if (span - p > j || j > span)
-        continue;
-
-      auto vj = directions[j];
-      for (size_t k = 0; k < n; ++k) // coefficient of w_k
-        if (span - p <= k && k <= span) {
-          if (k >= fix && k < n - fix)
-            A(i * nv + jv, k - fix) = der[2][k+p-span] * der[2][j+p-span];
-          b(i * nv + jv) -= curve.cp[k] * vj * der[2][k+p-span] * der[2][j+p-span];
-        }
-    }
+  // Boundary constraints
+  for (size_t i = 0; i < nb; ++i) {
+    A(boundary[i], n + i) = 1;
+    A(n + i, boundary[i]) = 1;
+    b(n + i) = boundary_weights[i];
   }
 
   // Solve the system
   Eigen::VectorXd x = A.colPivHouseholderQr().solve(b);
-  for (size_t i = fix; i < n - fix; ++i)
-    curve.cp[i] += directions[i] * x(i - fix);
+  for (size_t i = 0; i < nb; ++i) // to avoid minimal errors
+    x(boundary[i]) = boundary_weights[i];
+  for (size_t i = 0; i < n; ++i)
+    curve.cp[i] += directions[i] * x(i);
 }
--- test.cc
+++ test.cc
@@ -5,16 +5,18 @@
 int main(int argc, char **argv) {
   BSplineCurve curve;
   curve.p = 3;
-  curve.n = 4;
-  curve.knots = { 0, 0, 0, 0, 0.5, 1, 1, 1, 1 };
-  curve.cp = { {0.3239,   -1.54945,  0},
-               {0.712463, -0.347985, 0},
-               {1.84883,   0.421245, 0},
-               {3.71833,  -0.428571, 0},
-               {4.40015,  -1.41758,  1}
+  curve.n = 5;
+  curve.knots = { 0, 0, 0, 0, 15.0/40.0, 20.0/40.0, 1, 1, 1, 1 };
+  curve.cp = {
+    { 0, 10,  0},
+    {10, 10,  0},
+    {15,  0,  0},
+    {20,  0,  0},
+    {30, 10,  0},
+    {40, 10,  0}
   };
-  VectorVector dirs(5, {0, 0, 1});
-  optimize(curve, dirs, 100, 1);
+  VectorVector dirs(6, {1, 2, 0});
+  optimize(curve, dirs, 100, 2);
   for (const auto &cp : curve.cp)
     std::cout << cp.x << ' ' << cp.y << ' ' << cp.z << std::endl;
 }
